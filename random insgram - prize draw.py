4d394b4c3795d4f6a291c89bfeb06777cf48ddea
Leonardo Heckmann <114787699+leoheckmann@users.noreply.github.com>
2023-01-22 15:10:36 -0300

    Feito atualização no menu de opções afim de tornar o programa maisn intuitivo.

--- random insgram - prize draw.py
+++ random insgram - prize draw.py
@@ -9,29 +9,24 @@ def receber_user():
     random.shuffle(usuarios)
     return random.choice(usuarios)
 
-frase1 = ("Sorteio de usuários do Instagram")
+frase1 = ("                          Sorteio de usuários do Instagram!")
 print("=~=" * 30)
 
 print(f"\033[1;33m{frase1}")
 print("\033[0;0m=~=" * 30)
 
-users = int(input("\033[1;31mDigite 1 para cadastrar o usuário e 2 para finalizar e ir para o sorteio: "))
-print(" ")
+users = 1
 
 while users == 1:
     arr = (input("\033[1;33mInsira o usuário: "))
 
     cadastro_user(arr)
 
-    users = int(input("\033[1;31mDigite 1 para cadastrar o usuário e 2 para finalizar: "))
+    users = int(input("""\033[1;31m   Selecione a opção!
+( 1 ) \033[1;31mCadastrar outro usuário!
+( 2 ) \033[1;31mSortear o ganhador!
+Digite >>> """))
 
 if len(usuarios) > 0:
     print(f"\033[1;0mUsuários cadastrados para o sorteio: {usuarios}")
     print(f"\033[1;34mO ganhador do sorteio foi: {receber_user()}")
-
-
-
-
-
-
-
